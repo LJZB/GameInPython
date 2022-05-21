5008195af185791e0c17a7fdc10fb8e675e3e73f
Luis Javier Zuluaga Betancur <luiszuluaga90@gmail.com>
2022-05-21 16:52:08 -0500

    Lesson #48, 9:45 min

--- Pruebas.py
+++ Pruebas.py
@@ -3,25 +3,38 @@ import os
 os.system('cls')
 
 #Programacióon Orientada a Objetos
-
-#Sin usar POO
-baseSalary = 30000
-overtime = 10
-rate = 20
-
-def getWage(baseSalary, overtime, rate):
-    return baseSalary + (overtime*rate)
-
-print('Employee 1 total salary is {}'.format(getWage(baseSalary, overtime, rate)))
-
-#Usando POO
 class Employee():
-    baseSalary = 30000
-    overtime = 10
-    rate = 20
+    #Constructor
+    def __init__(self, baseSalary, overtime, rate):
+        self.baseSalary = baseSalary
+        self.overtime = overtime
+        self.rate = rate
 
     def getWage(self):
         return self.baseSalary + (self.overtime*self.rate)
 
-employe1 = Employee()
-print('Employee 1 total salary is {}'.format(employe1.getWage()))
+#Herencia
+class Engineer(Employee):
+    def __init__(self, baseSalary, overtime, rate):
+        self.baseSalary = baseSalary
+        self.overtime = overtime
+        self.rate = rate
+
+class Doctor(Employee):
+    def __init__(self, baseSalary, overtime, rate):
+        print(f"I am a doctor and my salary is {baseSalary}")
+
+Ross = Doctor(2000,23,70)
+
+# Mark = Engineer(1000,15,10)
+# print(Mark.getWage())
+
+# Harry = Engineer(1500,12,15)
+# print(Harry.getWage())
+
+
+#employee1 = Employee(20000,13,20) #Creación de objeto de la clase Employee
+#print('Employee 1 (POO) total salary is {}'.format(employee1.getWage()))
+
+#employee2 = Employee(25000,12,19)
+#print('Employee 2 (POO) total salary is {}'.format(employee2.getWage()))
